a6511ddc7c550741147b578bfd16dd43c2ab100d
Jason C <jforeza@gmail.com>
2018-06-05 11:33:58 -0700

    Commit initial project to repo

---
+++ app/src/main/java/perfectly/balanced/as_all_things_should_be/InfinityGauntlet.java
@@ -0,0 +1,13 @@
+package perfectly.balanced.as_all_things_should_be;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfinityGauntlet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infinity_gauntlet);
+    }
+}
