283e2a5c2bcdb5105c732619d5294603a4e6023c
Jason C <jforeza@gmail.com>
2018-07-02 16:55:53 -0700

    Support basic recycler view implementation with a random list of words

--- app/src/main/java/perfectly/balanced/as_all_things_should_be/InfinityGauntlet.java
+++ app/src/main/java/perfectly/balanced/as_all_things_should_be/InfinityGauntlet.java
@@ -2,44 +2,58 @@ package perfectly.balanced.as_all_things_should_be;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.RelativeLayout;
 
 
 import com.inmobi.ads.InMobiAdRequestStatus;
 import com.inmobi.ads.InMobiBanner;
-import com.inmobi.sdk.InMobiSdk;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.Map;
 
 
-public class InfinityGauntlet extends AppCompatActivity {
+public class InfinityGauntlet extends AppCompatActivity  {
+
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     static String TAG = "PERFBALANCE";
-
-    private InMobiBanner bannerAd;
     private InMobiBanner mBannerAd;
 
+    String [] myDataSet = new String [] {"glow stick","sofa","remote","seat belt","chocolate","doll","nail clippers","cookie jar","car","slipper","shovel","clay pot","buckel","headphones","shoes","keyboard","shoe lace","bananas","twezzers","controller","lip gloss","CD","vase","pen","blouse","glass","pencil","mouse pad","hanger","toothbrush","radio","tv","mp3 player","canvas","chapter book","cell phone","sand paper","tissue box","cat","stop sign","chalk","thread","candle","toe ring","air freshener","teddies","bag","eye liner","checkbook","scotch tape","lamp","conditioner","television","floor","book","ring","street lights","screw","twister","clamp","pants","towel","playing card","box","washing machine","truck","couch","shawl","white out","stockings","bow","grid paper","shampoo","cup","perfume","button","cork","photo album","spoon","sailboat","leg warmers","packing peanuts","monitor","candy wrapper","tooth picks","soy sauce packet","fork","paint brush","clock","model car","socks","clothes","credit card","ipod","needle","hair tie","zipper","bottle","plate","mop"};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        Log.d(TAG, "On create called");
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infinity_gauntlet);
 
+        // Find recycler view in XML
+        mRecyclerView = findViewById(R.id.my_recycler_view);
+        // mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+
+        mAdapter = new MyAdapter(myDataSet);
+        mRecyclerView.setAdapter(mAdapter);
 
         // Log.d(TAG, "Init SDK");
         // Get ref to adcontainer
-            RelativeLayout adContainer = (RelativeLayout) findViewById(R.id.ad_container);
-
+        RelativeLayout adContainer = (RelativeLayout) findViewById(R.id.ad_container);
 
         setupBannerAd(); // loads mBanner
+        Log.d(TAG, "On create finished");
+
 
     }
 
---
+++ app/src/main/java/perfectly/balanced/as_all_things_should_be/MyAdapter.java
@@ -0,0 +1,99 @@
+package perfectly.balanced.as_all_things_should_be;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Provide views to RecyclerView with data from mDataSet.
+ */
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private static final String TAG = "CustomAdapter";
+
+    private String[] mDataSet;
+
+    // BEGIN_INCLUDE(recyclerViewSampleViewHolder)
+    /**
+     * Provide a reference to the type of views that you are using (custom ViewHolder)
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+
+
+        private final TextView textView;
+
+        public ViewHolder(View v) {
+            super(v);
+
+
+            Log.d(TAG, "ViewHolder extends RecyclerView.ViewHolder create ");
+
+            // Define click listener for the ViewHolder's View.
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "Element " + getAdapterPosition() + " clicked.");
+                }
+            });
+            textView = (TextView) v.findViewById(R.id.textView);
+        }
+
+        public TextView getTextView() {
+            Log.d(TAG, "TextView getTextView");
+
+            return textView;
+        }
+    }
+    // END_INCLUDE(recyclerViewSampleViewHolder)
+
+    /**
+     * Initialize the dataset of the Adapter.
+     *
+     * @param dataSet String[] containing the data to populate views to be used by RecyclerView.
+     */
+    public MyAdapter(String[] dataSet) {
+        mDataSet = dataSet;
+        Log.d(TAG, "DataSet created");
+
+    }
+
+
+    // BEGIN_INCLUDE(recyclerViewOnCreateViewHolder)
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+
+        Log.d(TAG, "ViewHolder onCreateViewHolder");
+
+
+        // Create a new view.
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.text_row_item, viewGroup, false);
+
+
+
+        return new ViewHolder(v);
+    }
+    // END_INCLUDE(recyclerViewOnCreateViewHolder)
+
+    // BEGIN_INCLUDE(recyclerViewOnBindViewHolder)
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        Log.d(TAG, "Element " + position + " set.");
+
+        // Get element from your dataset at this position and replace the contents of the view
+        // with that element
+        viewHolder.getTextView().setText(mDataSet[position]);
+    }
+    // END_INCLUDE(recyclerViewOnBindViewHolder)
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataSet.length;
+    }
+}
